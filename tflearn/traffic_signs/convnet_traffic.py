4bc82a33e1ba42562bdb44eb013149688d5a9149
jimchen <jimchen1218@sina.com>
2017-09-21 00:07:35 +0800

    modified tflearn cnn train traffic_signal

--- tflearn/traffic_signs/convnet_traffic.py
+++ tflearn/traffic_signs/convnet_traffic.py
@@ -16,10 +16,9 @@ from tflearn.layers.estimator import regression
 import os
 import random
 import skimage.data
-
 import skimage.transform
-import matplotlib
-import matplotlib.pyplot as plt
+from skimage import color
+from PIL import Image
 import numpy as np
 import tensorflow as tf
 import time
@@ -27,32 +26,22 @@ import argparse
 import sys
 import tempfile
 import tflearn.datasets.mnist as mnist
-from skimage import color
-from PIL import Image
+
+
 
 print(__doc__)
 
-'''
-import tflearn.datasets.mnist as mnist  
-X, Y, testX, testY = mnist.load_data(one_hot=True)
-x_a = np.array(X)
-print("load_datasets X:%s"%(X))
-print("load_datasets Y:%s"%(Y))
-print("load_datasets testX:%s"%(testX.shape))
-print("load_datasets testY:%s"%(testY.shape))
-X = X.reshape([-1, 28, 28, 1])  
-testX = testX.reshape([-1, 28, 28, 1])  
-'''
 
 IMAGE_SIZE =32
 CLASS_LABELS =62
 DATASETS_PATH = "traffic_signs"
 train_data_dir = os.path.join(DATASETS_PATH,"datasets/BelgiumTS/Training")
 test_data_dir = os.path.join(DATASETS_PATH,"datasets/BelgiumTS/Testing")
+predict_image_dir = os.path.join(DATASETS_PATH,"1.ppm")
 
 def load_datasets(data_dir,img_crop_size):
 		directories = [d for d in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, d))]
-		print("load_datasets directories:%s"%(directories))
+		#print("load_datasets directories:%s"%(directories))
 		images = []
 		labels = []
 		for d in directories:
@@ -64,11 +53,10 @@ def load_datasets(data_dir,img_crop_size):
 						image=color.rgb2gray(image)
 						images.append(image)
 						id = int(d)
-						#print("\nid:%s"%id)
 						label =[0]*CLASS_LABELS
 						for i in range(CLASS_LABELS):
 								if i == id:
-										print("\ni==id i:%d,id:%d"%(i,id))
+										#print("\ni==id i:%d,id:%d"%(i,id))
 										label[i] = 1
 						#print("\nlabel:",label)
 						labels.append(label)
@@ -100,30 +88,24 @@ def load_dataset_traffic(path_dir):
 		labels_a = np.array(labels)
 		print("\nimages shape:", images_a.shape)
 		print("\nlabels shape:", labels_a.shape)
-		
 		images = images_a.reshape([-1,IMAGE_SIZE,IMAGE_SIZE,1])
 		print("\nimages shape:", images.shape)
 		return images,labels
 
 
-def convnet(num_classes):
-		network = input_data(shape=[None, IMAGE_SIZE, IMAGE_SIZE, 1], name='input')
-		
+def convnet(img_crop_size,num_classes):
+		network = input_data(shape=[None, img_crop_size, img_crop_size, 1], name='input')
 		# CNN
 		network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")  
 		network = max_pool_2d(network, 4)  
 		network = local_response_normalization(network)  
 		network = conv_2d(network, 64, 3, activation='relu', regularizer="L2")
 		network = max_pool_2d(network, 2)  
-		network = local_response_normalization(network)  
-				
+		network = local_response_normalization(network)
 		# Full connection
 		network = fully_connected(network, 128, activation='tanh')
 		network = dropout(network, 0.5)  
-		#network = fully_connected(network, 256, activation='tanh')  
-		#network = dropout(network, 0.5)  
 		network = fully_connected(network, num_classes, activation='softmax')
-		
 		# Regression
 		network = regression(network, optimizer='adam', learning_rate=0.01,
 		                     loss='categorical_crossentropy', name='target') 
@@ -131,53 +113,35 @@ def convnet(num_classes):
   
 
 def train(network, X, Y):
-    # Training
     model = tflearn.DNN(network)
     if os.path.isfile('model_convnet.model'):
         model.load('model_convnet.model')
     model.fit(X, Y, n_epoch=10, validation_set=0.1, shuffle=True,
-              show_metric=True, batch_size=64, snapshot_step=20,
+              show_metric=True, batch_size=64, snapshot_step=200,
               snapshot_epoch=False, run_id='convnet')
-    # Save the model
     model.save('model_convnet.model')
     return model
-  
-
-def predict(network, modelfile,images):
-    model = tflearn.DNN(network)
-    model.load(modelfile)
-    return model.predict(images)
 
 
-def load_image(img_path,img_crop_size):
+def load_predict_image(img_path,img_crop_size):
 		img = skimage.data.imread(img_path)
 		image = skimage.transform.resize(img, (img_crop_size,img_crop_size))
 		image=color.rgb2gray(image)
+		image = image.reshape([-1,IMAGE_SIZE,IMAGE_SIZE,1])
 		return image
 
 # Training
 if __name__ == '__main__':
-		#img_path = '02083_00000.ppm'
-		img_path = '00420_00002.ppm'
-		imgs = []
-		img = load_image(img_path,IMAGE_SIZE)
-		image = img.reshape([-1,IMAGE_SIZE,IMAGE_SIZE,1])
-		#imgs.append(pil_to_nparray(img))
 		images,labels = load_dataset_traffic(train_data_dir)
 		time_start = time.time()
-		net = convnet(CLASS_LABELS)
+		net = convnet(IMAGE_SIZE,CLASS_LABELS)
 		model = train(net,images,labels)
-		#predicted = predict(net, 'model_convnet.model',imgs)
-		#print(predicted)
-		pred = model.predict(image)
-		print("__main__ pred:%s"%pred[0][1])
+		predict_image = load_predict_image(predict_image_dir,IMAGE_SIZE)
+		pred = model.predict(predict_image)
+		print("\nmain pred:%s"%pred)
 		time_duration = time.time() - time_start
 		print("\nmodel training duration:%ss"%time_duration)
 		
-		#model = tflearn.DNN(network, tensorboard_verbose=0)
-		#model.fit({'input': images}, {'target': labels}, n_epoch=20,
-		#           validation_set=({'input': testX}, {'target': testY}), 
-		#           snapshot_step=10, show_metric=True, run_id='convnet_mnist')
 		
 
            
